23e3e3f21262b804b005d23142289d55b9d27e6a
Martin Čorovčák <martino.coro@gmail.com>
2023-02-10 16:14:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paint 2D
+
+A simple drawing application in Java using Swing and AWT libraries for Java Programming class.
---
+++ src/main/java/com/corovcak/martin/java/paint/Main.java
@@ -0,0 +1,7 @@
+package com.corovcak.martin.java.paint;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
